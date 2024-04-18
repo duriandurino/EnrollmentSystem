8fd46fa6f1632b5508019dda375e307f80192d70
Adrian Alejandrino <79866-23237761@SERVER2.DOMAIN>
2024-04-18 13:23:54 +0800

    partial code

--- EnrollmentSystem/Form1.Designer.cs
+++ EnrollmentSystem/Form1.Designer.cs
@@ -28,13 +28,228 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SubjectCodeTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.UnitsTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.CurriculumYearTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.CategoryComboBox = new System.Windows.Forms.ComboBox();
+            this.OfferingComboBox = new System.Windows.Forms.ComboBox();
+            this.CourseCodeComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 99);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Subject Code";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SubjectCodeTextBox
+            // 
+            this.SubjectCodeTextBox.Location = new System.Drawing.Point(175, 96);
+            this.SubjectCodeTextBox.Name = "SubjectCodeTextBox";
+            this.SubjectCodeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SubjectCodeTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 125);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Description";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Location = new System.Drawing.Point(175, 122);
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DescriptionTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 151);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Units";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UnitsTextBox
+            // 
+            this.UnitsTextBox.Location = new System.Drawing.Point(175, 148);
+            this.UnitsTextBox.Name = "UnitsTextBox";
+            this.UnitsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UnitsTextBox.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 189);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Offering";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(78, 215);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Category";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(78, 254);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Course Code";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(78, 280);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label7.Size = new System.Drawing.Size(81, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Curriculum Year";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CurriculumYearTextBox
+            // 
+            this.CurriculumYearTextBox.Location = new System.Drawing.Point(175, 277);
+            this.CurriculumYearTextBox.Name = "CurriculumYearTextBox";
+            this.CurriculumYearTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CurriculumYearTextBox.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(127, 39);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Subject Entry";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(520, 140);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // CategoryComboBox
+            // 
+            this.CategoryComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CategoryComboBox.FormattingEnabled = true;
+            this.CategoryComboBox.Items.AddRange(new object[] {
+            "LEC",
+            "LAB"});
+            this.CategoryComboBox.Location = new System.Drawing.Point(175, 215);
+            this.CategoryComboBox.Name = "CategoryComboBox";
+            this.CategoryComboBox.Size = new System.Drawing.Size(100, 21);
+            this.CategoryComboBox.TabIndex = 4;
+            // 
+            // OfferingComboBox
+            // 
+            this.OfferingComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.OfferingComboBox.FormattingEnabled = true;
+            this.OfferingComboBox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.OfferingComboBox.Location = new System.Drawing.Point(175, 186);
+            this.OfferingComboBox.Name = "OfferingComboBox";
+            this.OfferingComboBox.Size = new System.Drawing.Size(100, 21);
+            this.OfferingComboBox.TabIndex = 5;
+            // 
+            // CourseCodeComboBox
+            // 
+            this.CourseCodeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CourseCodeComboBox.FormattingEnabled = true;
+            this.CourseCodeComboBox.Location = new System.Drawing.Point(175, 250);
+            this.CourseCodeComboBox.Name = "CourseCodeComboBox";
+            this.CourseCodeComboBox.Size = new System.Drawing.Size(100, 21);
+            this.CourseCodeComboBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CourseCodeComboBox);
+            this.Controls.Add(this.OfferingComboBox);
+            this.Controls.Add(this.CategoryComboBox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.CurriculumYearTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.UnitsTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DescriptionTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SubjectCodeTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox SubjectCodeTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox UnitsTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox CurriculumYearTextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.ComboBox CategoryComboBox;
+        private System.Windows.Forms.ComboBox OfferingComboBox;
+        private System.Windows.Forms.ComboBox CourseCodeComboBox;
     }
 }
 
--- EnrollmentSystem/Form1.cs
+++ EnrollmentSystem/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,30 @@ namespace EnrollmentSystem
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\\Server2\second semester 2023-2024\LAB802\79866_CC_APPSDEV22_1200_0130_PM_TTH\79866-23237761\Desktop\FINALS\EnrollmentSystem\Alejandrino.accdb";
+
+            OleDbConnection thisConnection = new OleDbConnection(connectionString);
+            string sql = "SELECT * FROM SUBJECTFILE";
+            OleDbDataAdapter thisAdapter = new OleDbDataAdapter(sql, thisConnection);
+            OleDbCommandBuilder thisBuilder = new OleDbCommandBuilder(thisAdapter);
+
+            DataSet thisDataSet = new DataSet();
+            thisAdapter.Fill(thisDataSet, "SubjectFile");
+
+            DataRow thisRow = thisDataSet.Tables["SubjectFile"].NewRow();
+            thisRow["SFSUBJCODE"] = SubjectCodeTextBox.Text;
+            thisRow["SFSUBJDESC"] = DescriptionTextBox.Text;
+            thisRow["SFSUBJUNITS"] = Convert.ToInt16(UnitsTextBox.Text);
+            thisRow["SFSUBJCATEGORY"] = CategoryComboBox.Text;
+            thisRow["SFSUBJREGOFRNG"] = OfferingComboBox.Text;
+
+            thisDataSet.Tables["SubjectFile"].Rows.Add(thisRow);
+            thisAdapter.Update(thisDataSet, "SubjectFile");
+            MessageBox.Show("Entries Recorded");
         }
     }
 }
